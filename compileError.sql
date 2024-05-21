ca40763657295430ec0d67f0e9876408103e1426
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2024-05-21 17:34:05 +0530

    Update compileError.sql

--- compileError.sql
+++ compileError.sql
@@ -1,3 +1,1 @@
-CREATE TABLE t1 (a TINYINT NOT NULL, b CHAR(4));
-INSERT INTO t1 VALUES (300, 'abcd');
---In this example, the INSERT statement tries to insert a value of 300 into a TINYINT column, which has a maximum value of 127. This will cause a data truncation warning. 
+select abc from DUAL;
