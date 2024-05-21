7bbb7f6d789ac8e56a9e0630422a086032dbd797
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2024-05-21 07:07:14 +0530

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,5 +1,9 @@
-select * from student1;
+
+SPOOL report.txt
+--select * from student1;
 select CMS_ID , NAME from FD.CMS;
 select USER_NAME from FD.FD_USER;
 select CMS_ID , NAME from FD.CMS;
 --where CMS_ID=1001
+
+SPOOL OFF
