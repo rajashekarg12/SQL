1c6d9ce5c34cf4162a669bf76e77381174474d16
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2024-05-29 18:18:01 +0530

    Update selectWithProp.sql

--- selectWithProp.sql
+++ selectWithProp.sql
@@ -1,4 +1,4 @@
-select CMS_ID , NAME from FD.CMS;
-select USER_NAME from FD.FD_USER;
-select CMS_ID , NAME from FD.CMS;
-select * from ${{FD_PROJECT_NAME}};
+--select CMS_ID , NAME from FD.CMS;
+--select USER_NAME from FD.FD_USER;
+--select CMS_ID , NAME from FD.CMS;
+--select * from ${{FD_PROJECT_NAME}};
