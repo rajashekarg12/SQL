5d43a28ae0c641164eeb9ac0e91403710772166c
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2024-05-29 19:41:23 +0530

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,8 +1,7 @@
 --select * from student1;
-spool test56.txt
 
 select CMS_ID , NAME from FD.CMS;
 select USER_NAME from FD.FD_USER;
 select CMS_ID , NAME from FD.CMS1;
 --where CMS_ID=1001
-spool off
+
