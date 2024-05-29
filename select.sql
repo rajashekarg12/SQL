c72fdf31be0380a78aaf7b5e6a6a5c9e2f4a36cf
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2024-05-29 19:41:49 +0530

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,7 +1,7 @@
 --select * from student1;
 
 select CMS_ID , NAME from FD.CMS;
-select USER_NAME from FD.FD_USER;
 select CMS_ID , NAME from FD.CMS1;
+select USER_NAME from FD.FD_USER;
 --where CMS_ID=1001
 
