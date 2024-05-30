9aef0eb7b2672e9aa0840ad87eee8244e0406a46
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2024-05-30 14:49:21 +0530

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,7 +1,13 @@
 --select * from student1;
+spool rajreport.txt;
 
 select CMS_ID , NAME from FD.CMS;
-select CMS_ID , NAME from FD.CMS1;
+
+
+select CMS_ID , NAME from FD.CMS;
+
+
+
 select USER_NAME from FD.FD_USER;
 exit sql.exitcode
 --where CMS_ID=1001
