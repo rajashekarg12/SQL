cec0f004a580c0127493f40f80e073f515ab683b
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2024-06-02 17:57:59 +0530

    Create lin-select.sql

---
+++ lin-select.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS country_table987;
+CREATE TABLE country_table987 (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
