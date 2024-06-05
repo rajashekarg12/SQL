67aec3b0286596bcfcff49a26fd74e38b5999c6f
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2024-06-05 20:22:26 +0530

    Create showError.sql

---
+++ showError.sql
@@ -0,0 +1,25 @@
+-- Creating a package with an intentional error
+SET serveroutput on
+CREATE OR REPLACE PACKAGE test_package IS
+    PROCEDURE greet;
+END test_package;
+/
+
+-- Creating the package body with an intentional error
+CREATE OR REPLACE PACKAGE BODY test_package IS
+    PROCEDURE greet IS
+    BEGIN
+        -- Intentional error: misspelled 'dbms_output'
+        dbms_outpu.put_line('Hello, world!');
+    END greet;
+END test_package;
+/
+
+--Correcting the package body
+CREATE OR REPLACE PACKAGE BODY test_package IS
+    PROCEDURE greet IS
+    BEGIN
+        dbms_output.put_line('Hello, world!');
+   END greet;
+END test_package;
+/
