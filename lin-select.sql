e087bfef6bf110d6bbc2b1173277aae3c7d1c4bd
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2024-06-07 08:28:05 +0530

    Update lin-select.sql

--- lin-select.sql
+++ lin-select.sql
@@ -1,14 +1,5 @@
-DROP TABLE country_table987;
-CREATE TABLE country_table987 (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255)
-)
 
-
-CREATE TABLE country_table989 (
+CREATE TABLE country_table102 (
     PersonID int,
     LastName varchar(255),
     FirstName varchar(255),
