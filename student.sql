75f0ff67b2fbf758162c5cdc51a5de117ae7aaa5
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2024-06-19 17:31:42 +0530

    Update student.sql

--- student.sql
+++ student.sql
@@ -4,16 +4,17 @@ CREATE TABLE student (
     student_id INT PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    birth_date DATE,
-    major VARCHAR(50)
+  
 );
 -- Insert three rows into the student table
-INSERT INTO student (student_id, first_name, last_name, birth_date, major)
-VALUES (1, 'John', 'Doe', '2000-01-01', 'Computer Science');
+-- Insert three rows into the student table
+INSERT INTO student (student_id, first_name, last_name)
+VALUES (1, 'John', 'Doe');
 
-INSERT INTO student (student_id, first_name, last_name, birth_date, major)
-VALUES (2, 'Jane', 'Smith', '2001-02-02', 'Mathematics');
+INSERT INTO student (student_id, first_name, last_name)
+VALUES (2, 'Jane', 'Smith');
 
-INSERT INTO student (student_id, first_name, last_name, birth_date, major)
-VALUES (3, 'Emily', 'Johnson', '2002-03-03', 'Physics');
-commit;
+INSERT INTO student (student_id, first_name, last_name)
+VALUES (3, 'Emily', 'Johnson');
+
+COMMIT;
