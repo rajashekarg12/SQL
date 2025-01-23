e2aa8ca33b88ef1db6e5927135474a1574f55c72
rajashekarg12 <110624878+rajashekarg12@users.noreply.github.com>
2025-01-23 10:30:43 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,10 +1,1 @@
-DRO TABLE TEST_AV_892;
-
--- Step 1: Create the table with a column having a small size limit
-CREATE TABLE TEST_AV_892 (
-    COL_1 VARCHAR2(10) -- Column with a maximum length of 10 characters
-);
-
--- Step 2: Attempt to insert a value longer than the allowed length
-INSERT INTO TEST_AV_892 (COL_1) 
-VALUES ('THIS_IS_TOO_LONGllllllll')
+selec * from students;
