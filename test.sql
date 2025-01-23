40a0574e27aa0cfacf7c4075fa20e19bd66a59d3
rajashekarg12 <110624878+rajashekarg12@users.noreply.github.com>
2025-01-23 10:11:25 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,1 +1,8 @@
- select * from Fd.user
+-- Step 1: Create the table with a column having a small size limit
+CREATE TABLE TEST_AV_89 (
+    COL_1 VARCHAR2(10) -- Column with a maximum length of 10 characters
+);
+
+-- Step 2: Attempt to insert a value longer than the allowed length
+INSERT INTO TEST_AV_89 (COL_1) 
+VALUES ('THIS_IS_TOO_LONGllllllll'); -- 14 characters, exceeding the limit
