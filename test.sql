3f4f7ac055b824ee7322bfb7a82fa5d3e1afdc7d
rajashekarg12 <110624878+rajashekarg12@users.noreply.github.com>
2025-01-23 10:13:50 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -5,4 +5,4 @@ CREATE TABLE TEST_AV_89 (
 
 -- Step 2: Attempt to insert a value longer than the allowed length
 INSERT INTO TEST_AV_89 (COL_1) 
-VALUES ('THIS_IS_TOO_LONGllllllll'); -- 14 characters, exceeding the limit
+VALUES ('THIS_IS_TOO_LONGllllllll')
