3358620059674208786a5ba22866dde2c5308192
rajashekarg12 <110624878+rajashekarg12@users.noreply.github.com>
2025-10-14 16:54:25 +0530

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,13 +1,1 @@
-whenever sqlerror exit sql.sqlcode;
-
-select CMS_ID , NAME from FD.CMS;
-
-
-select CMS_ID , NAME from FD.CMS2;
-
-
-
-select USER_NAME from FD.FD_USER;
---where CMS_ID=1001
-
-exit;
+select * from dual;
